cbf071be23a92f9722c577c9f1e4bc7c3f7ef478
Yann Chemin <ychemin@gmail.com>
2016-04-01 12:43:56 +0200

    Updated Python code, data file and output screenshot

--- 2016_UR_OpenLog/display_rain.py
+++ 2016_UR_OpenLog/display_rain.py
@@ -1,34 +1,34 @@
 #!/usr/bin/env python
 
-# Data file name
+#Data file name
 f="MetDept1.csv"
 
-# Read CSV files
+#Read CSV files
 import csv
 
-# Open csv file
+# open csv file
 csvfile = open( f, "rb" )
 
-# Sniff into 10Kb of the file to check its dialect
+# sniff into 10KB of the file to check its dialect
 dialect = csv.Sniffer().sniff( csvfile.read( 10*1024 ) )
 csvfile.seek(0)
 
-# Read csv file according to dialect
+# read csv file according to dialect
 reader = csv.reader( csvfile, dialect )
 
-# Read header
-header = read.next()
+# read header
+header = reader.next()
 
-# Read data into memory
+# read data into memory
 data = [row for row in reader]
 
-# Close input file
+# close input file
 csvfile.close()
 
-print( header )
-print( data[-2][:] )
+print(header)
+print(data[-2][:])
 
-# Plot some data
+#Plot some data
 import datetime
 import matplotlib.dates as dlt
 X=[]
@@ -40,9 +40,34 @@ for i in range(len(data)):
 		day,month,year=data[:][i][4].split("/")
 		hour,minute,second=data[:][i][5].split(":")
 		try:
-			X.append(dlt.date2num(datetime.datetime(int(year),int(month),int(day),int(hour),int(minute),int(second))))
+			X.append(dlt.date2num(datetime.datetime(int(year), int(month), int(day), int(hour), int(minute), int(second))))
 		except:
 			X.append(0.0)
 	else:
 		X.append(0.0)
+
+print(header[16])
+Y=[]
+for i in range(len(data)):
+	if(i%1000 == 0):
+		print("Data",i,len(data))
+	#16 is rain hourly I think
+	if(data[:][i][16] != ''):
+		try:
+			Y.append(float(data[:][i][16]))
+		except:
+			Y.append(0.0)
+	else:
+		Y.append(0.0)
+
+print('graphing rain hourly')
+import numpy as np
+import matplotlib.pyplot as plt
+fig = plt.figure(figsize=(23.5, 13.0)) 
+plt.title("Accumulation of hourly rain (mm/h)")
+plt.ylabel("Rainfall (mm/h)")
+plt.xlabel("Time (days)")
+plt.plot(X[:-2],Y[:-2])
+fig.savefig('MWSDataAnalysis.png', facecolor=fig.get_facecolor(), transparent=True)
+fig.show()
 
